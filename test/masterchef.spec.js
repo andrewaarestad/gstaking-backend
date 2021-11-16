0152efec708643dea48ae7afc59f4e6db4be9d9b
Andrew Aarestad <andrew@aa-engineering.com>
2021-11-15 22:33:51 -0600

    basic tests for deploying RNG and MasterChef

--- test/masterchef.spec.js
+++ test/masterchef.spec.js
@@ -1,10 +1,24 @@
 const {TestCommon} = require("./test-common.js");
 const {expect} = require("chai");
+const {BigNumber} = require("ethers");
 
 describe('MasterChef Functionality', () => {
 
+  it('should deploy the RNG contract', async() => {
+    await TestCommon.deployRng();
+  });
+
+  /*
+         RNGToken _rngToken,
+        address _devaddr,
+        uint256 _rngPerBlock,
+        uint256 _startBlock,
+        uint256 _bonusEndBlock
+   */
   it('should deploy the MasterChef contract', async() => {
-    await TestCommon.deployMasterChef();
+    const rng = await TestCommon.deployRng();
+    const [owner, _] = await ethers.getSigners();
+    await TestCommon.deployMasterChef(rng.address, owner.address);
   });
 
 
--- test/test-common.js
+++ test/test-common.js
@@ -3,9 +3,16 @@ const {BigNumber} = require("ethers");
 const {ethers} = require("hardhat");
 
 class TestCommon {
-  static async deployMasterChef() {
+  static async deployRng() {
+    const contractFactory = await ethers.getContractFactory("RNGToken");
+    const contract = await contractFactory.deploy();
+    await contract.deployed();
+    return contract;
+  }
+
+  static async deployMasterChef(rngContract, devAddress) {
     const contractFactory = await ethers.getContractFactory("MasterChef");
-    const contract = await contractFactory.deploy("MasterChef", "MasterChef");
+    const contract = await contractFactory.deploy(rngContract, devAddress, BigNumber.from(1), BigNumber.from(1), BigNumber.from(1));
     await contract.deployed();
     return contract;
   }
