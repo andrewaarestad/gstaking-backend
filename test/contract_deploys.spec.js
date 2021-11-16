887fab02b9a076a80e0e7ed7d503fbe932b9a0f1
Andrew Aarestad <andrew@aa-engineering.com>
2021-11-15 22:44:04 -0600

    test for deploying the farms

--- test/contract_deploys.spec.js
+++ test/contract_deploys.spec.js
@@ -8,18 +8,19 @@ describe('MasterChef Functionality', () => {
     await TestCommon.deployRng();
   });
 
-  /*
-         RNGToken _rngToken,
-        address _devaddr,
-        uint256 _rngPerBlock,
-        uint256 _startBlock,
-        uint256 _bonusEndBlock
-   */
   it('should deploy the MasterChef contract', async() => {
     const rng = await TestCommon.deployRng();
     const [owner, _] = await ethers.getSigners();
     await TestCommon.deployMasterChef(rng.address, owner.address);
   });
+
+  it('should deploy the farms', async() => {
+    const rng = await TestCommon.deployRng();
+    // TODO: Replace erc721Address with deployed erc721 contract address, we are using rng.address for now which is invalid
+    await TestCommon.deployGausFarm(rng.address, rng.address);
+    await TestCommon.deployLootFarm(rng.address, rng.address);
+    await TestCommon.deployNFarm(rng.address, rng.address);
+  })
 
 
 })
--- test/test-common.js
+++ test/test-common.js
@@ -10,9 +10,30 @@ class TestCommon {
     return contract;
   }
 
-  static async deployMasterChef(rngContract, devAddress) {
+  static async deployMasterChef(rngContractAddress, devAddress) {
     const contractFactory = await ethers.getContractFactory("MasterChef");
-    const contract = await contractFactory.deploy(rngContract, devAddress, BigNumber.from(1), BigNumber.from(1), BigNumber.from(1));
+    const contract = await contractFactory.deploy(rngContractAddress, devAddress, BigNumber.from(1), BigNumber.from(1), BigNumber.from(1));
+    await contract.deployed();
+    return contract;
+  }
+
+  static async deployGausFarm(rngContractAddress, erc721Address) {
+    const contractFactory = await ethers.getContractFactory("GausFarm");
+    const contract = await contractFactory.deploy(rngContractAddress, erc721Address, BigNumber.from(1), BigNumber.from(1));
+    await contract.deployed();
+    return contract;
+  }
+
+  static async deployLootFarm(rngContractAddress, erc721Address) {
+    const contractFactory = await ethers.getContractFactory("LootFarm");
+    const contract = await contractFactory.deploy(rngContractAddress, erc721Address, BigNumber.from(1), BigNumber.from(1));
+    await contract.deployed();
+    return contract;
+  }
+
+  static async deployNFarm(rngContractAddress, erc721Address) {
+    const contractFactory = await ethers.getContractFactory("NFarm");
+    const contract = await contractFactory.deploy(rngContractAddress, erc721Address, BigNumber.from(1), BigNumber.from(1));
     await contract.deployed();
     return contract;
   }
